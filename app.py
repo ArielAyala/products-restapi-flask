9f23e808f7fe48443019dc5c70fa5aca854e6913
Ariel Ayala <arielayalamail@gmail.com>
2020-01-19 18:49:44 -0300

    Ruta get con parámetro

--- app.py
+++ app.py
@@ -14,5 +14,15 @@ def getProducts():
     return jsonify({"products": products, "message": "Product's List"})
 
 
+@app.route('/product/<string:product_name>')
+def getProduct(product_name):
+    productsFound = [
+        product for product in products if product['name'] == product_name]
+    if(len(productsFound) > 0):
+        return jsonify({"product": productsFound[0]})
+    return jsonify({"message": "Product not found"})
+    # return jsonify(product_name)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
