260cfe608a029489d358276f4dc0587051b967fd
Ariel Ayala <arielayalamail@gmail.com>
2020-01-19 18:08:26 -0300

    Agregando json de productos

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
+from products import products
 from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-from products import products
 
 @app.route('/ping')
 def ping():
-    return jsonify({"message":"pong"})
+    return jsonify({"message": "pong"})
+
 
 @app.route('/products')
 def getProducts():
-    return jsonify(products)
+    return jsonify({"products": products, "message": "Product's List"})
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=4000)
-
-
-
